38c949719fa9a815077a33deac869f66b23ed02d
rebs-code <jack.rebussi@gmail.com>
2024-03-01 19:59:23 +0100

    Update index.html and add AdvancedSearchPage.vue

---
+++ src/pages/AdvancedSearchPage.vue
@@ -0,0 +1,32 @@
+<script>
+
+export default {
+    name: 'AdvancedSearchPage',
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App Advanced search page!'
+        }
+    }
+};
+
+</script>
+
+<template>
+    <div class="home flex-grow-1">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "../scss/helpers/variables" as *;
+
+.home {
+    text-align: center;
+    background-color: $light-color;
+
+    h1 {
+        color: $primary-color;
+        margin-top: 100px;
+    }
+}
+</style>
