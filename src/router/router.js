85c587c4ac3bdbe94dcc02959ef10afc34eb48d8
rebs-code <jack.rebussi@gmail.com>
2024-03-01 20:09:43 +0100

    Add MessageSuccessPage component and update router.js

--- src/router/router.js
+++ src/router/router.js
@@ -3,11 +3,13 @@ import HomePage from '../pages/HomePage.vue'
 import AdvancedSearchPage from '../pages/AdvancedSearchPage.vue'
 import GameMasterPage from '../pages/GameMasterPage.vue'
 import ErrorPage from '../pages/ErrorPage.vue'
+import MessageSuccessPage from '../pages/MessageSuccessPage.vue'
 
 const routes = [
     { path: '/', name: 'home', component: HomePage },
     { path: '/advanced-search', name: 'advanced-search', component: AdvancedSearchPage },
     { path: '/game-master', name: 'game-master', component: GameMasterPage },
+    { path: '/message-success', name: 'message-success', component: MessageSuccessPage },
     { path: '/:pathMatch(.*)*', name: 'error', component: ErrorPage }
 ]
 
