cd0e5e279292159b062c9a33cc21e84c1e25ae6a
rebs-code <jack.rebussi@gmail.com>
2024-03-01 18:39:28 +0100

    add: HomePage placeholder text

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,24 @@
+<script>
+
+export default {
+    name: 'HomePage',
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App HOME PAGE!'
+        }
+    }
+};
+
+</script>
+
+<template>
+    <div class="home">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<style scoped>
+.home {
+    text-align: center;
+}
+</style>
