393e00b57db501d787f5964fab0e42e32b6dd02b
rebs-code <jack.rebussi@gmail.com>
2024-03-01 20:00:32 +0100

    Add AdvancedSearchPage to router

--- src/router/router.js
+++ src/router/router.js
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
+import AdvancedSearchPage from '../pages/AdvancedSearchPage.vue'
 
 const routes = [
-    { path: '/', name: 'home', component: HomePage }
+    { path: '/', name: 'home', component: HomePage },
+    { path: '/advanced-search', name: 'advanced-search', component: AdvancedSearchPage }
+
 ]
 
 const router = createRouter({
