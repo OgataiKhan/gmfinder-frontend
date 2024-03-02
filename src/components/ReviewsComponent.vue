5b5ecc98a547b8d49b01210254ad014415f10328
rebs-code <jack.rebussi@gmail.com>
2024-03-02 13:59:36 +0100

    create: reviews component

---
+++ src/components/ReviewsComponent.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'ReviewsComponent',
+    data() {
+        return {
+            msg: 'This is the review component!'
+        }
+    }
+};
+</script>
+
+<template>
+    <div class="reviews">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
