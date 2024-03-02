d67a4d1e4a949b0aea39cd23ecb9830962af285d
rebs-code <jack.rebussi@gmail.com>
2024-03-02 15:00:49 +0100

    Update PromotedMastersComponent and HomePage.vue

--- src/components/PromotedMastersComponent.vue
+++ src/components/PromotedMastersComponent.vue
@@ -13,9 +13,9 @@ export default {
     <div class="promoted-masters">
         <h1>{{ msg }}</h1>
         <div class="row">
-            <div class="col">
+            <div class="col col-lg-4">
                 <div class="card">
-                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+                    <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">This is a card with supporting text below as a natural lead-in to additional
