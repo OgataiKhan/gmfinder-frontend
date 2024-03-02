560a0fd09ae70b29e5ef3a2aa1bc3a2c4baa8f98
gaetano-94 <gaetano.panico1994@gmail.com>
2024-03-02 10:09:28 +0100

    feat: header and router-link

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,28 +1,94 @@
 <script>
-
-
 export default {
   name: 'AppHeader',
 };
-
 </script>
 
 <template>
   <header>
-    <div class="container">
-      Header
+    <div
+      class="container d-flex justify-content-between align-items-center py-2"
+    >
+      <!-- logo -->
+      <div class="logo">
+        <router-link
+          :to="{ name: 'home' }"
+          class="text-router text-decoration-none"
+        >
+          <img
+            src="/img/dungeons_and_dragons_logo_by_floodgrunt-d6my4z8.png"
+            alt=""
+        /></router-link>
+      </div>
+      <!-- /logo -->
+      <!-- navbar -->
+      <nav class="navbar-link">
+        <ul class="d-flex align-items-center gap-3">
+          <!-- link navbar -->
+          <li>
+            <router-link
+              :to="{ name: 'home' }"
+              class="text-router text-decoration-none link text-white"
+              ><strong>Home</strong></router-link
+            >
+          </li>
+          <li>
+            <router-link
+              :to="{ name: 'game-master' }"
+              class="text-router text-decoration-none link text-white"
+              ><strong>Game Master</strong></router-link
+            >
+          </li>
+          <li>
+            <router-link
+              :to="{ name: 'message-success' }"
+              class="text-router text-decoration-none link text-white"
+              ><strong>Message</strong></router-link
+            >
+          </li>
+          <li>
+            <router-link
+              :to="{ name: 'error' }"
+              class="text-router text-decoration-none link text-white"
+              ><strong>Error</strong></router-link
+            >
+          </li>
+          <li>
+            <router-link
+              :to="{ name: 'advanced-search' }"
+              class="btn bg-light px-2 py-1 rounded"
+              ><strong>Search</strong></router-link
+            >
+          </li>
+          <!-- /link navbar -->
+        </ul>
+      </nav>
+      <!-- /navbar -->
     </div>
   </header>
 </template>
 
 <style scoped lang="scss">
-@use "../scss/helpers/variables" as *;
-
+@use '../scss/helpers/variables' as *;
 
 header {
   background-color: $primary-color;
-  padding: 20px 0;
   width: 100%;
-  color: $light-color;
+}
+
+/* logo */
+.logo {
+  width: 65px;
+  height: 65px;
+}
+
+.logo img {
+  width: 100%;
+}
+/* navbar */
+.navbar-link ul {
+  list-style: none;
+  margin-top: 10px;
+  padding-left: 0;
 }
 </style>
