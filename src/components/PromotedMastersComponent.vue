33863948f09b738ab80bc8b5b773000101c7ed20
rebs-code <jack.rebussi@gmail.com>
2024-03-02 15:24:17 +0100

    Add carousel to PromotedMastersComponent.vue

--- src/components/PromotedMastersComponent.vue
+++ src/components/PromotedMastersComponent.vue
@@ -3,16 +3,33 @@ export default {
     name: 'PromotedMastersComponent',
     data() {
         return {
-            msg: 'This is a promoted masters component!'
+            msg: 'This is a promoted masters component!',
+            slides: [
+                {
+                    img: 'https://placehold.co/600x400',
+                    caption: 'First slide',
+                    text: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
+                },
+                {
+                    img: 'https://placehold.co/600x400',
+                    caption: 'Second slide',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+                },
+                {
+                    img: 'https://placehold.co/600x400',
+                    caption: 'Third slide',
+                    text: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
+                }
+            ]
         }
-    }
+    },
 };
 </script>
 
 <template>
     <div class="promoted-masters">
         <h1>{{ msg }}</h1>
-        <div class="row">
+        <!-- <div class="row">
             <div class="col col-lg-4">
                 <div class="card">
                     <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
@@ -24,6 +41,35 @@ export default {
                     </div>
                 </div>
             </div>
+        </div> -->
+        <div id="carouselExampleCaptions" class="carousel slide">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                    aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                    aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div v-for="(slide, index) in slides" :key="index" class="carousel-item" :class="{ active: index === 0 }">
+                    <img :src="slide.img" class="d-block w-100" :alt="slide.label">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>{{ slide.caption }}</h5>
+                        <p>{{ slide.text }}</p>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
     </div>
 </template>
