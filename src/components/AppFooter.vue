4f227c712dc089af4bb8485945b08bf2f29380ad
gaetano-94 <gaetano.panico1994@gmail.com>
2024-03-03 15:55:01 +0100

    feat: import footertop and footerbottom

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,28 +1,21 @@
 <script>
-
+import FooterTop from './footer/FooterTop.vue';
+import FooterBottom from './footer/FooterBottom.vue';
 
 export default {
   name: 'AppFooter',
+  components: {
+    FooterTop,
+    FooterBottom,
+  },
 };
-
 </script>
 
 <template>
   <footer>
-    <div class="container">
-      Footer
-    </div>
+    <FooterTop />
+    <FooterBottom />
   </footer>
 </template>
 
-<style scoped lang="scss">
-@use "../scss/helpers/variables" as *;
-
-
-footer {
-  background-color: $primary-color;
-  padding: 20px 0;
-  width: 100%;
-  color: $light-color;
-}
-</style>
+<style scoped lang="scss"></style>
