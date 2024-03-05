aae342fe5972575300c01ed9b61400dd1f00dadd
rebs-code <jack.rebussi@gmail.com>
2024-03-05 16:50:00 +0100

    feat: responsive navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,52 +6,41 @@ export default {
 
 <template>
   <header>
-    <div
-      class="container-fluid d-flex justify-content-between align-items-center py-2"
-    >
+    <div class="container-fluid d-flex justify-content-between align-items-center py-2">
       <!-- logo -->
-      <div class="logo">
+      <!-- <div class="logo">
         <router-link :to="{ name: 'home' }" class="text-decoration-none">
-          <img
-            src="/img/dungeons_and_dragons_logo_by_floodgrunt-d6my4z8.png"
-            alt=""
-        /></router-link>
-      </div>
+          <img src="/img/dungeons_and_dragons_logo_by_floodgrunt-d6my4z8.png" alt="" /></router-link>
+      </div> -->
       <!-- /logo -->
       <!-- navbar -->
-      <nav class="navbar-link">
-        <ul class="d-flex align-items-center gap-3">
-          <!-- link navbar -->
-          <li>
-            <router-link :to="{ name: 'home' }" class="link"
-              ><strong>Home</strong></router-link
-            >
-          </li>
-          <li>
-            <router-link :to="{ name: 'game-master' }" class="link"
-              ><strong>Game Master</strong></router-link
-            >
-          </li>
-          <li>
-            <router-link :to="{ name: 'message-success' }" class="link"
-              ><strong>Message</strong></router-link
-            >
-          </li>
-          <li>
-            <router-link :to="{ name: 'error' }" class="link"
-              ><strong>Error</strong></router-link
-            >
-          </li>
-          <li>
-            <router-link
-              :to="{ name: 'advanced-search' }"
-              id="button"
-              class="btn"
-              ><strong>Search</strong></router-link
-            >
-          </li>
-          <!-- /link navbar -->
-        </ul>
+      <nav class="navbar navbar-expand-md flex-grow-1 text-center">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" id="header-button"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 gap-3">
+              <li class="nav-item">
+                <router-link :to="{ name: 'home' }" class="nav-link">Home</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link :to="{ name: 'home' }" class="nav-link">Games</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link :to="{ name: 'home' }" class="nav-link">FAQ</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link :to="{ name: 'home' }" class="nav-link">Contact</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link :to="{ name: 'home' }" class="nav-link">Search</router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
       <!-- /navbar -->
     </div>
@@ -75,33 +64,35 @@ header {
 .logo img {
   width: 100%;
 }
+
 /* navbar */
-.navbar-link ul {
-  list-style: none;
-  padding-left: 0;
+
+.navbar {
+  background-color: $primary-color;
 }
 
-.link {
+.nav-item:hover {
+  background-color: $primary-color;
+  filter: brightness(1.4);
+  border-radius: 5px;
+}
+
+.nav-link {
   text-decoration: none;
   color: white;
+  // margin-right: 10px;
 }
 
-.link:hover {
-  color: $contrast-color;
-}
-
-#button {
+#header-button {
   background-color: $light-color;
   color: $primary-color;
   border: 1px solid $light-color;
-  padding: 5px 15px;
-  border-radius: 5px;
   cursor: pointer;
   transition: background-color 0.3s;
   font-weight: bold;
 }
 
-#button:hover {
+#header-button:hover {
   background-color: $contrast-color;
   color: $light-color;
   border-color: $contrast-color;
