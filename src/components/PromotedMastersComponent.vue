fe3a890d77d2566d10318f212f397bf5266689d7
rebs-code <jack.rebussi@gmail.com>
2024-03-05 12:04:55 +0100

    Add title to PromotedMastersComponent.vue

--- src/components/PromotedMastersComponent.vue
+++ src/components/PromotedMastersComponent.vue
@@ -3,66 +3,49 @@ export default {
     name: 'PromotedMastersComponent',
     data() {
         return {
-            msg: 'This is a promoted masters component!',
-            slides: [
-                {
-                    img: 'https://placehold.co/600x400',
-                    caption: 'First slide',
-                    text: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
-                },
-                {
-                    img: 'https://placehold.co/600x400',
-                    caption: 'Second slide',
-                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-                },
-                {
-                    img: 'https://placehold.co/600x400',
-                    caption: 'Third slide',
-                    text: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
-                },
-                {
-                    img: 'https://placehold.co/600x400',
-                    caption: 'Fourth slide',
-                    text: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
-                }
-            ],
-
+            title: 'Featured Masters of the Realm',
             masters: [
 
                 {
                     id: 1,
                     name: 'Master 1',
                     description: 'This is the first master',
+                    picture: 'https://via.placeholder.com/150',
                     // Add more master details here
                 },
                 {
                     id: 2,
                     name: 'Master 2',
                     description: 'This is the second master',
+                    picture: 'https://via.placeholder.com/150',
                     // Add more master details here
                 },
                 {
                     id: 3,
                     name: 'Master 3',
                     description: 'This is the third master',
+                    picture: 'https://via.placeholder.com/150',
                     // Add more master details here
                 },
                 {
                     id: 4,
                     name: 'Master 4',
                     description: 'This is the fourth master',
+                    picture: 'https://via.placeholder.com/150',
                     // Add more master details here
                 },
                 {
                     id: 5,
                     name: 'Master 5',
                     description: 'This is the fifth master',
+                    picture: 'https://via.placeholder.com/150',
                     // Add more master details here
                 },
                 {
                     id: 6,
                     name: 'Master 6',
                     description: 'This is the sixth master',
+                    picture: 'https://via.placeholder.com/150',
                     // Add more master details here
                 },
                 {
@@ -161,37 +144,9 @@ export default {
 
 <template>
     <div class="promoted-masters container">
-        <h1>{{ msg }}</h1>
-
-
-        <!-- <div id="carouselExampleCaptions" class="carousel slide">
-            <div class="carousel-indicators">
-                <button type="button" v-for="(slide, idx) in slides" :key="'indicator-' + idx"
-                    data-bs-target="#carouselExampleCaptions" :data-bs-slide-to="idx" :class="{ active: idx === 0 }"
-                    :aria-label="'Slide ' + (idx + 1)"></button>
-            </div>
-            <div class="carousel-inner">
-                <div v-for="(slide, index) in slides" :key="index" class="carousel-item" :class="{ active: index === 0 }">
-                    <img :src="slide.img" class="d-block w-100" :alt="slide.label">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>{{ slide.caption }}</h5>
-                        <p>{{ slide.text }}</p>
-                    </div>
-                </div>
-            </div>
-            
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
-                data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
-                data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div> -->
-
+        <div class="title">
+            <h3 class="text-center py-3">{{ title }}</h3>
+        </div>
         <div>
             <div class="masters-grid container">
                 <div class="row g-3">
