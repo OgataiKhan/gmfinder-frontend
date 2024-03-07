8298a98bc9721b450469198e3b224112a594c06f
gaetano-94 <gaetano.panico1994@gmail.com>
2024-03-07 17:05:25 +0100

    feat: bold back to top footer

--- src/components/ButtonBackTopComponent.vue
+++ src/components/ButtonBackTopComponent.vue
@@ -1,48 +1,46 @@
 <script>
 export default {
-    name: 'ButtonBackTopComponent',
-    data() {
-        return {
-            active: false
-        };
-    },
-    // aggiunta del metodo allo scroll di window
-    mounted() {
-        window.addEventListener('scroll', this.setScroll);
-    },
+  name: 'ButtonBackTopComponent',
+  data() {
+    return {
+      active: false,
+    };
+  },
+  // aggiunta del metodo allo scroll di window
+  mounted() {
+    window.addEventListener('scroll', this.setScroll);
+  },
 
-    methods: {
-        // imposta i pixel scrollati
-        setScroll() {
-            this.active = window.scrollY;
-        },
-        // fa ritornare la pagina in cima
-        scrollToTop() {
-            window.scrollTo({
-                top: 0,
-                behavior: 'smooth'
-            });
-        }
-    }
+  methods: {
+    // imposta i pixel scrollati
+    setScroll() {
+      this.active = window.scrollY;
+    },
+    // fa ritornare la pagina in cima
+    scrollToTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    },
+  },
 };
 </script>
 
 <template>
-    <button class="scroll-to-top" v-if="active" @click="scrollToTop">
-        <span class="arrow-up">Back To Top</span>
-    </button>
+  <button class="scroll-to-top" v-if="active" @click="scrollToTop">
+    <span class="arrow-up"><strong>Back To Top</strong></span>
+  </button>
 </template>
-
-
 
 <style lang="scss" scoped>
 @use '../scss/helpers/variables' as *;
 
 .scroll-to-top {
-    z-index: 999;
-    cursor: pointer;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  z-index: 999;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
