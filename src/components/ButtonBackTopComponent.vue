9aa0b24c3effc32c32b9e76af05a4cfe161c238a
rebs-code <jack.rebussi@gmail.com>
2024-03-07 14:16:02 +0100

    feat: back to top button

---
+++ src/components/ButtonBackTopComponent.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+    name: 'ButtonBackTopComponent',
+    data() {
+        return {
+            active: false
+        };
+    },
+    // aggiunta del metodo allo scroll di window
+    mounted() {
+        window.addEventListener('scroll', this.setScroll);
+    },
+
+    methods: {
+        // imposta i pixel scrollati
+        setScroll() {
+            this.active = window.scrollY;
+        },
+        // fa ritornare la pagina in cima
+        scrollToTop() {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        }
+    }
+};
+</script>
+
+<template>
+    <button class="scroll-to-top" v-if="active" @click="scrollToTop">
+        <span class="arrow-up">Back To Top</span>
+    </button>
+</template>
+
+
+
+<style lang="scss" scoped>
+@use '../scss/helpers/variables' as *;
+
+.scroll-to-top {
+    z-index: 999;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
