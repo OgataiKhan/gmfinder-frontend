5987401b6599608dbdc2f7c215998e2206045da2
gaetano-94 <gaetano.panico1994@gmail.com>
2024-03-08 09:18:00 +0100

    feat: button page

--- src/pages/AdvancedSearchPage.vue
+++ src/pages/AdvancedSearchPage.vue
@@ -11,7 +11,6 @@ export default {
       store,
       apiResponse: null,
     };
-
   },
   components: {
     SearchComponent,
@@ -33,13 +32,19 @@ export default {
     nextPage() {
       if (this.store.currentPage < this.store.lastPage) {
         this.store.currentPage++;
-        this.$refs.SearchComponent.searchGm(this.store.selectedGameSystem, this.store.currentPage);
+        this.$refs.SearchComponent.searchGm(
+          this.store.selectedGameSystem,
+          this.store.currentPage
+        );
       }
     },
     previousPage() {
       if (this.store.currentPage > 1) {
         this.store.currentPage--;
-        this.$refs.SearchComponent.searchGm(this.store.selectedGameSystem, this.store.currentPage);
+        this.$refs.SearchComponent.searchGm(
+          this.store.selectedGameSystem,
+          this.store.currentPage
+        );
       }
     },
   },
@@ -67,16 +72,25 @@ export default {
           <ul class="d-flex gap-3 flex-column">
             <li v-for="gm in store.gameMastersResults" :key="gm.id">
               <!-- route link f9or clickable card -->
-              <router-link :to="{ name: 'game-master' }" class="nav-link" @click="selectGm(gm)">
+              <router-link
+                :to="{ name: 'game-master' }"
+                class="nav-link"
+                @click="selectGm(gm)"
+              >
                 <!-- create card for each gm -->
                 <div class="card d-flex flex-md-row">
                   <div class="card-header border-bottom-0">
-                    <img :src="gm.profile_img
-            ? this.store.api.baseURL +
-            this.store.api.apiUrls.storage +
-            gm.profile_img
-            : '/img/generic-avatar.jpg'
-            " class="card-img-top" alt="profile pic" />
+                    <img
+                      :src="
+                        gm.profile_img
+                          ? this.store.api.baseURL +
+                            this.store.api.apiUrls.storage +
+                            gm.profile_img
+                          : '/img/generic-avatar.jpg'
+                      "
+                      class="card-img-top"
+                      alt="profile pic"
+                    />
                   </div>
                   <div class="card-body">
                     <div class="text-center text-md-start">
@@ -84,20 +98,20 @@ export default {
                     </div>
                     <hr />
                     <div>
-                      <h6>
-                        Game Systems
-                      </h6>
-                      <p><span v-for="(system, index) in gm.game_systems" :key="index">
+                      <h6>Game Systems</h6>
+                      <p>
+                        <span
+                          v-for="(system, index) in gm.game_systems"
+                          :key="index"
+                        >
                           {{ system.name
-                          }}{{ index < gm.game_systems.length - 1 ? ', ' : '' }} </span>
+                          }}{{ index < gm.game_systems.length - 1 ? ', ' : '' }}
+                        </span>
                       </p>
                     </div>
                     <div>
-                      <h6>
-                        Max Players
-                      </h6>
-                      <p>{{ gm.max_players }}
-                      </p>
+                      <h6>Max Players</h6>
+                      <p>{{ gm.max_players }}</p>
                     </div>
                   </div>
                 </div>
@@ -110,16 +124,24 @@ export default {
       </div>
       <!-- pagination -->
       <nav class="mb-3">
-        <ul class="pagination d-flex justify-content-between">
+        <ul class="pagination d-flex justify-content-between px-3 pb-3">
           <!-- Previous -->
           <li>
-            <div class="page-item" v-if="apiResponse && this.$route.query.page" v-show="apiResponse.results?.prev_page_url">
+            <div
+              class="page-item"
+              v-if="apiResponse && this.$route.query.page"
+              v-show="apiResponse.results?.prev_page_url"
+            >
               <button class="page-link" @click="previousPage">Previous</button>
             </div>
           </li>
           <!-- Next -->
           <li>
-            <div class="page-item" v-if="apiResponse && this.$route.query.page" v-show="apiResponse.results?.next_page_url">
+            <div
+              class="page-item"
+              v-if="apiResponse && this.$route.query.page"
+              v-show="apiResponse.results?.next_page_url"
+            >
               <button class="btn btn-info" @click="nextPage">Next</button>
             </div>
           </li>
@@ -127,7 +149,6 @@ export default {
       </nav>
       <!-- /pagination -->
     </div>
-
   </main>
 </template>
 
