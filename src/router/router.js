2dedcfb9aa1369811514ac74497b20f5ae764f25
rebs-code <jack.rebussi@gmail.com>
2024-03-09 14:19:17 +0100

    Fix navigation and add scroll behavior

--- src/router/router.js
+++ src/router/router.js
@@ -17,7 +17,11 @@ const routes = [
 
 const router = createRouter({
     history: createWebHistory(),
-    routes
+    routes,
+    //add scroll behavior
+    scrollBehavior() {
+        return { x: 0, y: 0 }
+    }
 })
 
 export default router
