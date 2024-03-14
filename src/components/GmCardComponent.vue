5d13f9f59c08824167693bebd5274592c99740e2
rebs-code <jack.rebussi@gmail.com>
2024-03-14 10:03:15 +0100

    feat: fix reviews count show

--- src/components/GmCardComponent.vue
+++ src/components/GmCardComponent.vue
@@ -163,13 +163,9 @@ export default {
             <i class="bi bi-stars"></i></span>
         </h4>
         <!-- send message icon -->
-        <div
-          class="d-flex align-items-center px-2 msg-send"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasMessage"
-          aria-controls="offcanvasMessage"
-          v-if="gmShow&&this.store.selectedGameMaster.is_available"
-        >
+        <div class="d-flex align-items-center px-2 msg-send" data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasMessage" aria-controls="offcanvasMessage"
+          v-if="gmShow && this.store.selectedGameMaster.is_available">
           <p class="mb-0 me-2">Send a Message</p>
           <i class="bi bi-send-fill gm-icons fs-3"></i>
         </div>
@@ -200,7 +196,7 @@ export default {
         </p>
         <p v-else>None yet</p>
       </div>
-      <div>
+      <div v-if="!gmShow">
         <h6>Reviews</h6>
         <p v-if="gm.reviews_count">{{ gm.reviews_count }}</p>
         <p v-else>None yet</p>
